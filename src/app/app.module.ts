f6bcd05c52ecbef55aee1d37bcf18826ce887a44
vladimir <vladimir.lazarchuk@computools.com>
2019-06-20 11:23:34 +0300

    project cleared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyCounterComponent } from './my-counter/my-counter.component';
-import { counterReducer } from './counter-redux';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        MyCounterComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        StoreModule.forRoot({count: counterReducer})
     ],
     providers: [],
     bootstrap: [AppComponent]
